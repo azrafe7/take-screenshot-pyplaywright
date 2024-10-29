9fe86de062fe4b1e471b5c9dd3d5deae33bc66a3
Giuseppe Di Mauro <azrafe7@gmail.com>
2024-10-29 17:49:38 +0100

    try again

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/playwright
+
 pip install -r requirements.txt
 playwright install
 
-export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/playwright
+#ls $PLAYWRIGHT_BROWSERS_PATH
 
 # Store/pull Playwright cache with build cache
-if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH ]]; then 
-  echo "...Copying Playwright Cache from Build Cache" 
-  cp -R $XDG_CACHE_HOME/playwright/ $PLAYWRIGHT_BROWSERS_PATH
-else 
-  echo "...Storing Playwright Cache in Build Cache" 
-  cp -R $PLAYWRIGHT_BROWSERS_PATH $XDG_CACHE_HOME
-fi
+#if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH ]]; then 
+#  echo "...Copying Playwright Cache from Build Cache" 
+#  cp -R $XDG_CACHE_HOME/playwright/ $PLAYWRIGHT_BROWSERS_PATH
+#else 
+#  echo "...Storing Playwright Cache in Build Cache" 
+#  cp -R $PLAYWRIGHT_BROWSERS_PATH $XDG_CACHE_HOME
+#fi
