f3d769a4e33e13f3a2027f51c49d475e080837a7
Giuseppe Di Mauro <azrafe7@gmail.com>
2024-10-29 17:29:20 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ set -e
 pip install -r requirements.txt
 playwright install
 
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/playwright
+
 # Store/pull Playwright cache with build cache
-if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH]]; then 
-  export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/playwright
+if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH ]]; then 
   echo "...Copying Playwright Cache from Build Cache" 
   cp -R $XDG_CACHE_HOME/playwright/ $PLAYWRIGHT_BROWSERS_PATH
 else 
