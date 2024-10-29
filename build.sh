1ca931d8a47a749e2f8661e1491ffd149baff9f7
Giuseppe Di Mauro <azrafe7@gmail.com>
2024-10-29 16:36:10 +0100

    build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+pip install -r requirements
+playwright install
+
+# Store/pull Playwright cache with build cache
+if [[! -d $PLAYWRIGHT_BROWSERS_PATH]]; then 
+  export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/playwright
+  echo "...Copying Playwright Cache from Build Cache" 
+  cp -R $XDG_CACHE_HOME/playwright/ $PLAYWRIGHT_BROWSERS_PATH
+else 
+  echo "...Storing Playwright Cache in Build Cache" 
+  cp -R $PLAYWRIGHT_BROWSERS_PATH $XDG_CACHE_HOME
+fi
