70e3d06655e355e7b3f5ed1d597351bf11703bee
Giuseppe Di Mauro <azrafe7@gmail.com>
2024-10-30 13:32:44 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/ms-playwright
+#export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/ms-playwright
 
 pip install -r requirements.txt
 playwright install
@@ -13,17 +13,17 @@ echo cache ms-playwright
 ls /opt/render/.cache/ms-playwright -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
 echo
 echo chrome ms-playwright
-ls /opt/render/.cache/ms-playwright/chromium-1140/chrome-linux/chrome/*.*
+#ls /opt/render/.cache/ms-playwright/chromium-1140/chrome-linux/chrome/*.*
 
 #ls /opt/render/project/playwright
 
-#ls $PLAYWRIGHT_BROWSERS_PATH
+ls $PLAYWRIGHT_BROWSERS_PATH
 
 # Store/pull Playwright cache with build cache
-#if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH ]]; then 
-#  echo "...Copying Playwright Cache from Build Cache" 
-#  cp -R $XDG_CACHE_HOME/playwright/ $PLAYWRIGHT_BROWSERS_PATH
-#else 
-#  echo "...Storing Playwright Cache in Build Cache" 
-#  cp -R $PLAYWRIGHT_BROWSERS_PATH $XDG_CACHE_HOME
-#fi
+if [[ ! -d $PLAYWRIGHT_BROWSERS_PATH ]]; then 
+  echo "...Copying Playwright Cache from Build Cache" 
+  cp -R $XDG_CACHE_HOME/playwright/ $PLAYWRIGHT_BROWSERS_PATH
+else 
+  echo "...Storing Playwright Cache in Build Cache" 
+  cp -R $PLAYWRIGHT_BROWSERS_PATH $XDG_CACHE_HOME
+fi
